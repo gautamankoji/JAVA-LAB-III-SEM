618c4932d50bf85ceef93f8a98c3e332f547eb82
Gautam Ankoji <120157430+gautamankoji@users.noreply.github.com>
2023-12-05 20:58:52 +0530

    Update DaemonThreads.java

--- Exercise 10 (Threads)/DaemonThreads.java
+++ Exercise 10 (Threads)/DaemonThreads.java
@@ -18,7 +18,7 @@ class DaemonThreads {
     A a1 = new A();
     A a2 = new A();
     A a3 = new A();
-    a1.setDaemon(true);
+    a2.setDaemon(true);
     a1.start();
     a2.start();
     a3.start();
