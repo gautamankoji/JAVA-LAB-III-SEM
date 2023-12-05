b59031549570c2d1b6fb3ffdc05455bfbf974325
Gautam Ankoji <120157430+gautamankoji@users.noreply.github.com>
2023-12-05 22:04:27 +0530

    Update ImportPackage.java

--- Exercise 12 (Package)/ImportPackage.java
+++ Exercise 12 (Package)/ImportPackage.java
@@ -5,3 +5,27 @@
  * Tuesday 05-12-2023 16:53:47 
 */
 
+// save by A.java 
+
+package pack; 
+
+public class A { 
+  public void msg(int a, int b) {
+    System.out.println("SUM: " + (a+b));
+  } 
+}
+
+// save by B.java 
+
+package mypack; 
+import pack.A; 
+
+class B { 
+  public static void main(String args[]) { 
+    A obj = new A();
+    int a = 792;
+    int b = 3728;
+    obj.add(a, b); 
+  } 
+} 
+
